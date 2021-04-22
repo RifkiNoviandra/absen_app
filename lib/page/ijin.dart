867aad761e7eb5d1503b9bd239561ec3bf5b78e8
Olfatalatas <olfatharits@gmail.com>
2021-04-22 11:22:59 +0700

    finish upload file

--- lib/page/ijin.dart
+++ lib/page/ijin.dart
@@ -1,6 +1,4 @@
 import 'package:absen_app/page/home.dart';
-import 'package:absen_app/page/profile.dart';
-import 'package:absen_app/page/riwayat.dart';
 import 'package:flutter/material.dart';
 
 class IzinPage extends StatefulWidget {
@@ -9,20 +7,8 @@ class IzinPage extends StatefulWidget {
 }
 
 class _IzinPageState extends State<IzinPage> {
-  int _currentIndex = 0;
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final List<Widget> _children = [
-      HomePage(),
-      RiwayatPage(),
-      ProfilePage(),
-    ];
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       extendBodyBehindAppBar: false,
@@ -49,27 +35,6 @@ class _IzinPageState extends State<IzinPage> {
             backgroundColor: Colors.teal.shade600,
           ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.teal.shade600,
-        currentIndex: _currentIndex,
-        onTap: onTabTapped,
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.home_outlined, size: 33),
-            title: new Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.folder_open_outlined, size: 33),
-            title: new Text('Riwayat'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.account_circle_outlined, size: 33),
-            title: new Text('Profile'),
-          ),
-        ],
-        elevation: 0,
-        backgroundColor: Colors.white,
       ),
       backgroundColor: Colors.teal.shade600,
       body: Center(
@@ -179,11 +144,15 @@ class BodyIzin extends StatelessWidget {
             Padding(
               padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 10.0),
               child: TextField(
+                showCursor: true,
+                readOnly: true,
                 decoration: InputDecoration(
                   labelText: 'unggah file disini',
                   suffixIcon: IconButton(
                     icon: Icon(Icons.cloud_upload_sharp),
-                    onPressed: () {},
+                    onPressed: () {
+                      _bottomSheet(context);
+                    },
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderRadius: const BorderRadius.all(
@@ -252,4 +221,277 @@ class Absen extends StatelessWidget {
       fit: BoxFit.fill,
     );
   }
+}
+
+_bottomSheet(context) {
+  showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 500,
+          decoration: BoxDecoration(
+              color: Colors.teal.shade600,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+          child: SingleChildScrollView(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 23.0, 0.0, 10.0),
+                      child: Text(
+                        "",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 10.0),
+                      child: SizedBox(
+                        width: 100.0,
+                        height: 80.0,
+                        child: RaisedButton(
+                          color: Colors.white,
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 10.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.file_copy_outlined, size: 50),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                      child: Text(
+                        "File",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                      child: SizedBox(
+                        width: 100.0,
+                        height: 80.0,
+                        child: RaisedButton(
+                          color: Colors.white,
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 10.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.photo_outlined, size: 50),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                      child: Text(
+                        "Foto",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 23.0, 0.0, 10.0),
+                      child: Text(
+                        "Upload New",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 10.0),
+                      child: SizedBox(
+                        width: 100.0,
+                        height: 80.0,
+                        child: RaisedButton(
+                          color: Colors.white,
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 10.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.camera_alt_outlined, size: 50),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                      child: Text(
+                        "Camera",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                      child: SizedBox(
+                        width: 100.0,
+                        height: 80.0,
+                        child: RaisedButton(
+                          color: Colors.white,
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 10.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.file_present, size: 50),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                      child: Text(
+                        "Dokumen",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 23.0, 0.0, 10.0),
+                      child: Text(
+                        "",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 10.0),
+                      child: SizedBox(
+                        width: 100.0,
+                        height: 80.0,
+                        child: RaisedButton(
+                          color: Colors.white,
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 10.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.video_call_outlined, size: 50),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                      child: Text(
+                        "Video",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                      child: SizedBox(
+                        width: 100.0,
+                        height: 80.0,
+                        child: RaisedButton(
+                          color: Colors.white,
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 10.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.drive_folder_upload, size: 50),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                      child: Text(
+                        "Drive",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      });
 }
