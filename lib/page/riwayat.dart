37b4236e23b97fa12d9c3653679a44bbad8042db
Olfatalatas <olfatharits@gmail.com>
2021-04-22 11:25:02 +0700

    update riwayat

--- lib/page/riwayat.dart
+++ lib/page/riwayat.dart
@@ -1,10 +1,79 @@
 import 'package:flutter/material.dart';
 
-class RiwayatPage extends StatelessWidget {
-  const RiwayatPage({Key key}) : super(key: key);
+class RiwayatPage extends StatefulWidget {
+  @override
+  _RiwayatPageState createState() => _RiwayatPageState();
+}
+
+class _RiwayatPageState extends State<RiwayatPage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      extendBodyBehindAppBar: false,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(150.0),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(15.0, 15.0, 20.0, 0.0),
+          child: AppBar(
+            centerTitle: true,
+            automaticallyImplyLeading: true,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () => Navigator.pop(context, false),
+              color: Colors.white,
+            ),
+            title: Container(
+              padding: EdgeInsets.all(20),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      backgroundColor: Colors.teal.shade600,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: BodyRiwayat(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BodyRiwayat extends StatelessWidget {
+  const BodyRiwayat({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: double.infinity,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(0),
+            topRight: Radius.circular(0),
+          )),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
   }
 }
