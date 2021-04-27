c4b9e32fcc4ff8321f0905019d3fefe3c5b3a6b4
RifkiNoviandra <69407359+RifkiNoviandra@users.noreply.github.com>
2021-04-27 21:11:30 +0700

    update

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,13 +1,32 @@
+import 'package:absen_app/API_code/loginAPI.dart';
 import 'package:absen_app/page/home.dart';
 import 'package:absen_app/page/register.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
+Future<UserLogin> userLogin(String email, String password) async {
+  final String apiUrl = "http://localhost:8080/user/login";
+  var response = await http
+      .post(Uri.parse(apiUrl), body: {"email": email, "password": password});
+
+  if (response.statusCode == 200) {
+    final String responseString = response.body;
+
+    return userLoginFromJson(responseString);
+  } else {
+    return null;
+  }
+}
+
 class _LoginPageState extends State<LoginPage> {
+  UserLogin _user;
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
   bool _showPassword = false;
   @override
   Widget build(BuildContext context) {
@@ -35,6 +54,7 @@ class _LoginPageState extends State<LoginPage> {
               Padding(
                 padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
                 child: TextField(
+                  controller: emailController,
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
                     prefixIcon: Icon(Icons.email_outlined),
@@ -59,6 +79,7 @@ class _LoginPageState extends State<LoginPage> {
               Padding(
                 padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 30.0),
                 child: TextField(
+                  controller: passwordController,
                   obscureText: !this._showPassword,
                   decoration: InputDecoration(
                     prefixIcon: Icon(Icons.vpn_key),
@@ -106,7 +127,18 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     color: Colors.white,
                     textColor: Colors.black,
-                    onPressed: () {
+                    onPressed: () async {
+                      final String email = emailController.text;
+                      final String password = passwordController.text;
+
+                      final UserLogin user = await userLogin(email, password);
+
+                      setState(() {
+                        _user = user;
+                      });
+
+                      if ("fetch message from API" ==
+                          "Your Email Isn't Registered") {}
                       Navigator.of(context).push(MaterialPageRoute(
                         builder: (context) => HomePage(),
                       ));
